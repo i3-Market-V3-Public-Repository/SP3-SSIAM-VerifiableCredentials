15f60467c1b3e26a7e8028d5837e1528c765b395
gobberr <rupert.gobber@yahoo.it>
2021-09-20 08:45:44 +0000

    Update IssuerRegistry.sol

--- smart-contract-registry/IssuerRegistry.sol
+++ smart-contract-registry/IssuerRegistry.sol
@@ -2,23 +2,23 @@ pragma solidity ^0.5.8;
 
 contract IssuerRegistry {
 
-    mapping (bytes32 => mapping (address => uint)) private issuers;
+    mapping (address => uint) private issuers;
     
 
     function addIssuer(address _wallet) public {
-        require (issuers[_wallet][0] == 0);
-        issuers[_wallet][0] = block.number;
+        require (issuers[_wallet] == 0);
+        issuers[_wallet] = block.number;
         emit Trusted(msg.sender, _wallet);
     }
     
     function removeIssuer(address _wallet) public {
-        require (issuers[_wallet][0] == 0);
-        issuers[_wallet][0] = block.number;
+        require (issuers[_wallet] == 0);
+        issuers[_wallet] = block.number;
         emit Trusted(msg.sender, _wallet);
     }
 
     function isTrusted(address _wallet) public view returns (bool) {
-        return issuers[_wallet][0];
+        return issuers[_wallet];
     }
     
     event Trusted(address truster, address issuer);
